2fb82be742e3e8037478db0a66ee5f2b50ebd6d0
lukas.pupelis <lukas.pupelis@insoft.lt>
2025-02-13 11:19:05 +0200

    Globalus CORS config'as.

--- app/src/main/java/lt/example/TimestampController.java
+++ app/src/main/java/lt/example/TimestampController.java
@@ -1,13 +1,11 @@
 package lt.example;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 public class TimestampController {
 
