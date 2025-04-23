f9261528bf2a13b84b08456f1df2367bd638aa5e
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2025-04-23 14:59:29 +0300

    Update PhotoListDto.java

--- app/src/main/java/lt/example/dtos/PhotoListDto.java
+++ app/src/main/java/lt/example/dtos/PhotoListDto.java
@@ -16,7 +16,6 @@ public class PhotoListDto {
     private String description;
     private String thumbnail;
     private List<String> tags;
-    private Object stuff;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
